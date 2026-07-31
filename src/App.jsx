131a80588030d745139d57c59fdc686b4341a72d
thomas <thomaslazzeri3@gmail.com>
2026-07-31 20:10:56 +0200

    feat: refactor App component to improve structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={HomePage} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
