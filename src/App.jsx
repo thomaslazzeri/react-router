6db9fc19284e53111c48ae3254b53ae03c697b55
thomas <thomaslazzeri3@gmail.com>
2026-07-31 21:28:56 +0200

    refactor: simplify JSX structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
@@ -6,17 +7,15 @@ function App() {
   const [state, setState] = useState(0);
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={(<>
-          <button onClick={() => setState(state => state + 1)}>Incrementa {state}</button>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/products">Prodotti</NavLink>
-        </>)} />
-        <Route path="/products" element={<>Prodotti</>} />
-        <Route path="products" element={<>Prodotti</>} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={(<>
+        <button onClick={() => setState(state => state + 1)}>Incrementa {state}</button>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Prodotti</NavLink>
+      </>)} />
+      <Route path="/products" element={<>Prodotti</>} />
+      <Route path="products" element={<>Prodotti</>} />
+    </Routes>
   );
 }
 
