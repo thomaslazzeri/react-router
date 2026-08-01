9388f19dfb8b2a066428554d343cda6fb72375bb
thomas <thomaslazzeri3@gmail.com>
2026-08-01 15:07:57 +0200

    feat: implement loading state for Products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,14 +5,19 @@ const API_URL = 'https://fakestoreapi.com/products';
 
 export const Products = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         axios.get(API_URL)
             .then(response => {
                 setProducts(response.data);
             })
             .catch(error => {
                 console.error("Si è verificato un errore nel caricamento dei dati:", error);
+            })
+            .finally(() => {
+                setLoading(false);
             });
     }, []);
 
@@ -20,7 +25,8 @@ export const Products = () => {
         <div className="products-page">
             <h1>I nostri Prodotti</h1>
             <div className="products">
-                {products.map(item => (
+                {loading && (<div>Sto caricando...</div>)}
+                {!loading && products.map(item => (
                     <div key={item.id} className="product-card">
                         <img src={item.image} alt={item.title} />
                         <h3>{item.title}</h3>
