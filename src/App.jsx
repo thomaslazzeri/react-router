00ce7f3b4c8dc1b736b9aff78dee8156069f365a
thomas <thomaslazzeri3@gmail.com>
2026-08-01 10:35:17 +0200

    feat: add header and navigation links to App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,23 @@ function App() {
   const [state, setState] = useState(0);
 
   return (
-    <Routes>
-      <Route path="/" element={(<>
-        <button onClick={() => setState(state => state + 1)}>Incrementa {state}</button>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/products">Prodotti</NavLink>
-      </>)} />
-      <Route path="/products" element={<>Prodotti</>} />
-      <Route path="products" element={<>Prodotti</>} />
-    </Routes>
+    <>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/products">Prodotti</NavLink>
+          <NavLink to="/us">Chi siamo</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={(
+          <>
+            <button onClick={() => setState(state => state + 1)}>Incrementa {state}</button>
+          </>)} />
+        <Route path="products" element={<>Prodotti</>} />
+        <Route path="us" element={<>Chi siamo</>} />
+      </Routes>
+    </>
   );
 }
 
